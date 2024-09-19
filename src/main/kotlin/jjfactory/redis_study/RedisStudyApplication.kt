c884e3acac342c92b9dad0050682cd460830b8fa
jaehyeong <wogud1514@gmail.com>
2024-09-19 14:28:51 +0900

    init

---
+++ src/main/kotlin/jjfactory/redis_study/RedisStudyApplication.kt
@@ -0,0 +1,11 @@
+package jjfactory.redis_study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RedisStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<RedisStudyApplication>(*args)
+}
---
+++ src/test/kotlin/jjfactory/redis_study/RedisStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package jjfactory.redis_study
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RedisStudyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
